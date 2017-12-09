dac4ca843a7c64a66433dd2f3b16a9fba9d10e01
j4ever66 <jordanever14@gmail.com>
2017-12-09 00:08:27 -0800

    Basix

---
+++ js/lib/custom.js
@@ -0,0 +1,37 @@
+$(document).ready(start);
+
+var _popSemaphore = false;
+
+
+
+function start(){
+	AllocateClickHandlers();
+
+
+}
+
+
+
+
+
+
+function AllocateClickHandlers(){
+	$(".popper").on("click", popPopper);
+
+
+}
+
+
+ 
+
+function popPopper(){
+	if(_popSemaphore){
+		$("nav.primary ul").slideUp("slow");
+		$(".popper").text("+");
+	}else{
+		$("nav.primary ul").slideDown("slow");
+		$(".popper").text("-");
+	}
+	_popSemaphore = !_popSemaphore;
+	console.log("poop"); 
+}
